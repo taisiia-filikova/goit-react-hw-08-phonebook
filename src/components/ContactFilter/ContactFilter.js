8c1d84cde0c894218c0b2b0f8219753e9bb32c27
taisiia-filikova <filikova@gmail.com>
2021-02-06 02:49:05 +0200

    Меняет компонент фильтр

--- src/components/ContactFilter/ContactFilter.js
+++ src/components/ContactFilter/ContactFilter.js
@@ -2,17 +2,16 @@ import { useSelector, useDispatch } from 'react-redux';
 import { contactsActions, contactsSelectors } from '../../redux/contacts';
 import { motion, AnimatePresence } from 'framer-motion';
 import { variants } from '../../utils/motionVar';
-
 import s from './ContactFilter.module.css';
 
 function ContactFilter() {
   const dispatch = useDispatch();
   const filter = useSelector(contactsSelectors.getFilter);
   const contacts = useSelector(contactsSelectors.getContacts);
-  const error = useSelector(contactsSelectors.getError);
+
   return (
     <>
-      {contacts.length > 0 && !error && (
+      {contacts.length > 0 && (
         <AnimatePresence>
           <label className={s.label}>
             <motion.input
@@ -34,4 +33,5 @@ function ContactFilter() {
     </>
   );
 }
+
 export default ContactFilter;
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,11 +1,10 @@
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { contactsOperations, contactsSelectors } from '../../redux/contacts';
-import Cleave from 'cleave.js/react';
 import { toast } from 'react-toastify';
+import NumberFormat from 'react-number-format';
 
 import Loader from '../Loader/Loader';
-
 import s from './ContactForm.module.css';
 
 function ContactForm() {
@@ -46,20 +45,14 @@ function ContactForm() {
     return name.trim() === '' || number.trim() === '';
   };
 
-  const checkValidNumber = number => {
-    return !/\d{3}[-]\d{2}[-]\d{2}/g.test(number);
-  };
-
   const handleSubmit = evt => {
     evt.preventDefault();
     if (checkRepeatName(name)) {
-      toast(`${name} is already in contacts.`);
+      return toast(`${name} is already in phonebook.`);
     } else if (checkRepeatNumber(number)) {
-      toast(`${number} is already in contacts.`);
+      return toast(`${number} is already in phonebook.`);
     } else if (checkEmptyQuery(name, number)) {
-      toast.info('Enter the name and phone number!');
-    } else if (checkValidNumber(number)) {
-      toast.error('Enter the correct phone number!');
+      return toast.info('Enter the name and phone number!');
     } else {
       dispatch(contactsOperations.addContact(name, number));
     }
@@ -86,21 +79,25 @@ function ContactForm() {
       </label>
       <label className={s.label}>
         Number
-        <Cleave
-          options={{ delimiter: '-', blocks: [3, 2, 2] }}
+        <NumberFormat
+          placeholder="Enter the phone number"
+          format="(###) ###-##-##"
+          mask="_"
+          pattern="^[0-9\s\(\)\-]{15}"
           type="tel"
           name="number"
           value={number}
           onChange={handleChange}
           className={s.input}
-          placeholder="555-55-55"
         />
       </label>
+
       {!isLoading && (
         <button className={s.button} type="submit">
           Add contact
         </button>
       )}
+
       {isLoading && <Loader />}
     </form>
   );
