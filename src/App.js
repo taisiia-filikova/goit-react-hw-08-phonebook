008d6433aba58912651dca50709319cadf404640
taisiia-filikova <filikova@gmail.com>
2021-02-06 02:45:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
-import Container from './components/Container/Container';
-import ContactForm from './components/ContactForm/ContactForm';
-import ContactList from './components/ContactList/ContactList';
-import ContactFilter from './components/ContactFilter/ContactFilter';
+import { lazy, Suspense, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { authorizationOperations } from './redux/authorization';
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import PublicRoute from './utils/publicRoute';
+import PersonRoute from './utils/personRoute';
+
+import Container from './components/Container/Container';
+import AppBar from './components/AppBar/AppBar';
+import Loader from './components/Loader/Loader';
+
+const HomeView = lazy(() => import('./views/HomeView/HomeView'));
+
+const RegisterView = lazy(() => import('./views/RegisterView/RegisterView'));
+const LoginView = lazy(() => import('./views/LoginView/LoginView'));
+const ContactsView = lazy(() => import('./views/ContactsView/ContactsView'));
+
+const NotFoundView = lazy(() => import('./views/NotFoundView/NotFoundView'));
+
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(authorizationOperations.getCurrentUser());
+  }, [dispatch]);
+
   return (
     <Container>
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <h2>Contacts</h2>
-      <ContactFilter />
-      <ContactList />
+      <AppBar />
+
+      <Suspense fallback={<Loader />}>
+        <Switch>
+          <PublicRoute path="/" exact>
+            <HomeView />
+          </PublicRoute>
+
+          <PublicRoute path="/register" restricted redirectTo="/contacts">
+            <RegisterView />
+          </PublicRoute>
+
+          <PublicRoute path="/login" restricted redirectTo="/contacts">
+            <LoginView />
+          </PublicRoute>
+
+          <PersonRoute path="/contacts" redirectTo="/login">
+            <ContactsView />
+          </PersonRoute>
+
+          <Route>
+            <NotFoundView />
+          </Route>
+        </Switch>
+      </Suspense>
+
       <ToastContainer autoClose={3700} position="top-center" />
     </Container>
   );
